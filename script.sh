42128952cfd670b7eee6f009c281023ac5b77bfb
Luciano Rodrigues da Silva <lurodrisilva@hotmail.com>
2019-11-04 17:50:12 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget http://download.redis.io/releases/redis-5.0.5.tar.gz
+tar zxvf redis-5.0.5.tar.gz
+cd redis-5.0.5
+make MALLOC=libc all
+make install
+
+/usr/local/bin/redis-server
